5bf0ad75beda9cc9410647b40b6562e06f9d734f
Michał Banach <michal_banach@outlook.com>
2020-07-28 00:10:25 +0200

    [front] Change endpoint to azure.

--- Worker.Api/ClientApp/src/environments/environment.prod.ts
+++ Worker.Api/ClientApp/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiEndpoint: 'https://localhost:5001',
+  apiEndpoint: 'https://workerapi20200727232326.azurewebsites.net',
   AuthZeroDomain: 'dev-zuy3w25z.eu.auth0.com',
   AuthZeroClientId: 'l9CfTyTsB9ml15KNrWFRuNXs06NZ7e7I',
   AuthZeroApiId: 'https://adiamicroserviceapi.com'
